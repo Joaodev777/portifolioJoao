1019d9430a791582e6cec24598619da4f6b042fd
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-06-24 10:08:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ $(document).ready(function() {
     $('.html .progress-bar-h').animate({ width: '100%' }, 2000, function() {
       $(this).siblings('.progress-text-h').text('100%');
     });
-  
+
     // Animação CSS
     $('.css .progress-bar').animate({ width: '80%' }, 2000, function() {
       $(this).siblings('.progress-text').text('80%');
     });
-  
+
     // Animação JavaScript
     $('.js .progress-bar').animate({ width: '90%' }, 2000, function() {
       $(this).siblings('.progress-text').text('90%');
     });
   });
-  
+
+  const progress = document.querySelector('.progress-done');
+
+setTimeout(() => {
+  progress.style.width = progress.getAttribute('data-done') + '%';
+  progress.style.opacity = 1;
+}, 100);
