5c3b8c9aa83eaf253c28fb5431736ca9e250d555
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-06-26 17:51:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -310,12 +310,12 @@
                         <div class="div">
                             <div data-bs-toggle="modal" data-bs-target="#modalsaiba"
                                 class="btn-gtx gradient-border mb-5 btn-side">
-                                <button class="btn-dark btn-general-m1 text-light" style="width: 150px"><a
+                                <button class="btn-dark btn-general-m1 text-light" style="width: 250px"><a
                                         data-bs-toggle="modal" data-bs-target="#modalsaiba" style="text-align: center;">Saiba
                                         mais</a></button>
                             </div>
                             <div href="#contato" class=" btn-gtx gradient-border mb-5 btn-con btn-side btn-si">
-                                <button class="btn-dark btn-general-m1 text-light" style="width: 200px;"><a
+                                <button class="btn-dark btn-general-m1 text-light" style="width: 250px;"><a
                                         href="#contato" style="text-align: center;">Faça seu
                                         orçamento</a></button>
                             </div>
