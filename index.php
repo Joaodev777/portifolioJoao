a8676abbe0bce6146929107c7f89941a29ab3bae
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-07-03 13:35:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -311,12 +311,13 @@
                                         src="img/icons8-github-48.png" alt=""></a>
                             </div>
                             <div class="div">
-                               
+                                 <div href="#contato" class=" btn-gtx gradient-border mb-5 btn-con btn-side btn-si" style="widht:250px"    >
                                     <a data-bs-toggle="modal" data-bs-target="#modalsaiba"
                                         style="text-align: center;"><button class="btn-dark btn-general-m1 btn-gtx gradient-border mb-5 btn-side text-light"
                                             style="width: 250px">Saiba
                                             mais</button></a>
-                                <div href="#contato" class=" btn-gtx gradient-border mb-5 btn-con btn-side btn-si">
+                                 </div>
+                              <div href="#contato" class=" btn-gtx gradient-border mb-5 btn-con btn-side btn-si" style="widht:250px"    >
                                     <a href="#contato" style="text-align: center;"><button
                                             class="btn-dark btn-general-m1 text-light" style="width: 250px;">Faça seu
                                             orçamento</button></a>
