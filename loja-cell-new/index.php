388080c27d969b0eb8adced963b605a0d16eed7b
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-07-15 08:58:38 -0300

    Update index.php

--- loja-cell-new/index.php
+++ loja-cell-new/index.php
@@ -111,7 +111,7 @@ $balance = $totalIncome - $totalExpense;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/logo-off.png" type="image/x-icon">
 
     <style>
         .color-green {
@@ -370,7 +370,7 @@ $balance = $totalIncome - $totalExpense;
     <!-- Barra lateral -->
     <nav class="navbar navbar-expand-lg position-fixed" id="nav" style="width: 100%; top: 0; z-index: 9999;">
         <div class="container-fluid">
-            <a class="navbar-brand text-light" style="font-size: 30px" href="#home"><img src="logo.png"></a>
+            <a class="navbar-brand text-light" style="font-size: 30px" href="#home"><img src="img/logo-off.png"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
@@ -475,7 +475,7 @@ $balance = $totalIncome - $totalExpense;
         </div>
 
         <div class="mt-4 print-section">
-            <h2>Transações -  <img src="/logo.png"></h2>
+            <h2>Transações -  <img src="img/logo-off.png"></h2>
             <?php if (count($filteredTransactions) > 0) : ?>
 
                 <table class="table" id="trans">
