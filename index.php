6cd9ca787d47b57a14596a15c5b7c750b59743c2
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-08-21 16:36:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 🌐 Site: https://newfiber.com.br
 📱 WhatsApp: https://wa.me/553732269519
 📸 Instagram: @newfiber_
-<br>
+<br><br>
 **Terça-feira:**
 🌐 Navegue com confiança e velocidade com a NewFiber! Nossos planos variados oferecem a flexibilidade que você precisa. Aproveite nosso plano de 500 Mbps por R$99,90 para uma conexão surpreendente. Ou, se preferir, experimente nosso plano de 650 Mbps por R$119,99 e eleve sua experiência online. E para quem busca uma opção mais econômica, temos o plano de 100 Mbps por R$80. Além de velocidade, todos os planos acima de 500 Mbps incluem acesso à TV Premium para seu entretenimento. E com nosso aplicativo NewFiber SAC, pagamentos e manutenções são mais simples do que nunca.
 
@@ -14,7 +14,7 @@
 📱 WhatsApp: https://wa.me/553732269519
 📸 Instagram: @newfiber_
 
-<br>
+<br><br>
 **Quarta-feira:**
 📺 Quarta-feira é o dia perfeito para experimentar a NewFiber! Seja com nosso plano de 500 Mbps por R$99,90, nosso plano de 650 Mbps por R$119,99 ou nosso plano de 100 Mbps por R$80, você terá a velocidade que precisa para suas atividades online. Além disso, todos os planos acima de 500 Mbps incluem acesso à TV Premium para entretenimento completo. E agora, com o novo aplicativo NewFiber SAC, você pode gerenciar pagamentos e solicitações de manutenção de forma conveniente.
 
@@ -22,7 +22,7 @@
 🌐 Site: https://newfiber.com.br
 📱 WhatsApp: https://wa.me/553732269519
 📸 Instagram: @newfiber_
-<br>
+<br><br>
 **Quinta-feira:**
 💥 Quinta-feira é sinônimo de escolha com a NewFiber! Nossos planos oferecem opções para todos os gostos. Aproveite nosso plano de 500 Mbps por R$99,90 para uma conexão incrível. Ou, se busca ainda mais velocidade, experimente nosso plano de 650 Mbps por R$119,99. E para quem procura uma opção econômica, temos o plano de 100 Mbps por R$80. E não importa qual plano você escolher, todos os planos acima de 500 Mbps incluem acesso à TV Premium para seu entretenimento. Não esqueça de conferir nosso novo aplicativo NewFiber SAC para pagamentos e manutenções simples.
 
@@ -30,7 +30,7 @@
 🌐 Site: https://newfiber.com.br
 📱 WhatsApp: https://wa.me/553732269519
 📸 Instagram: @newfiber_
-<br>
+<br><br>
 
 **Sexta-feira:**
 🎉 Sextou com opções incríveis da NewFiber! Escolha entre nosso plano de 500 Mbps por R$99,90 para uma navegação de alta velocidade. Ou, para uma experiência ainda mais intensa, experimente nosso plano de 650 Mbps por R$119,99. E para quem procura um ótimo custo-benefício, temos o plano de 100 Mbps por R$80. E para completar, todos os planos acima de 500 Mbps incluem acesso à TV Premium para entretenimento sem limites. Não deixe de conhecer nosso novo aplicativo NewFiber SAC, que torna pagamentos e solicitações de manutenção mais fáceis do que nunca.
