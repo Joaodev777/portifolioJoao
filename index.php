7d6d7e39e0290cebae8eb8c2693459b1d9ccaf61
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-08-24 13:44:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,4 @@
-**Segunda-feira:**
-🚀 Comece a semana com a NewFiber e descubra uma nova era de conectividade! Nosso plano de 500 Mbps por apenas R$99,90 oferece uma internet ultra-rápida para todas as suas atividades online. E não é só isso – temos o plano de 650 Mbps por R$119,99 e o plano de 100 Mbps por R$80. Além de velocidade, ao escolher qualquer um desses planos, você terá acesso à TV Premium para entretenimento sem limites. Quer mais? Experimente nosso novo aplicativo NewFiber SAC para pagamentos e solicitações de manutenção simplificadas.
+🕒 Atendimento: Seg-Sex, 8h às 18h
+📞 Suporte: (37) 3226-9519
 
-📞 Ligue: (37) 3226-9519
-🌐 Site: https://newfiber.com.br
-📱 WhatsApp: https://wa.me/553732269519
-📸 Instagram: @newfiber_
-<br><br>
-**Terça-feira:**
-🌐 Navegue com confiança e velocidade com a NewFiber! Nossos planos variados oferecem a flexibilidade que você precisa. Aproveite nosso plano de 500 Mbps por R$99,90 para uma conexão surpreendente. Ou, se preferir, experimente nosso plano de 650 Mbps por R$119,99 e eleve sua experiência online. E para quem busca uma opção mais econômica, temos o plano de 100 Mbps por R$80. Além de velocidade, todos os planos acima de 500 Mbps incluem acesso à TV Premium para seu entretenimento. E com nosso aplicativo NewFiber SAC, pagamentos e manutenções são mais simples do que nunca.
-
-📞 Ligue: (37) 3226-9519
-🌐 Site: https://newfiber.com.br
-📱 WhatsApp: https://wa.me/553732269519
-📸 Instagram: @newfiber_
-
-<br><br>
-**Quarta-feira:**
-📺 Quarta-feira é o dia perfeito para experimentar a NewFiber! Seja com nosso plano de 500 Mbps por R$99,90, nosso plano de 650 Mbps por R$119,99 ou nosso plano de 100 Mbps por R$80, você terá a velocidade que precisa para suas atividades online. Além disso, todos os planos acima de 500 Mbps incluem acesso à TV Premium para entretenimento completo. E agora, com o novo aplicativo NewFiber SAC, você pode gerenciar pagamentos e solicitações de manutenção de forma conveniente.
-
-📞 Ligue: (37) 3226-9519
-🌐 Site: https://newfiber.com.br
-📱 WhatsApp: https://wa.me/553732269519
-📸 Instagram: @newfiber_
-<br><br>
-**Quinta-feira:**
-💥 Quinta-feira é sinônimo de escolha com a NewFiber! Nossos planos oferecem opções para todos os gostos. Aproveite nosso plano de 500 Mbps por R$99,90 para uma conexão incrível. Ou, se busca ainda mais velocidade, experimente nosso plano de 650 Mbps por R$119,99. E para quem procura uma opção econômica, temos o plano de 100 Mbps por R$80. E não importa qual plano você escolher, todos os planos acima de 500 Mbps incluem acesso à TV Premium para seu entretenimento. Não esqueça de conferir nosso novo aplicativo NewFiber SAC para pagamentos e manutenções simples.
-
-📞 Ligue: (37) 3226-9519
-🌐 Site: https://newfiber.com.br
-📱 WhatsApp: https://wa.me/553732269519
-📸 Instagram: @newfiber_
-<br><br>
-
-**Sexta-feira:**
-🎉 Sextou com opções incríveis da NewFiber! Escolha entre nosso plano de 500 Mbps por R$99,90 para uma navegação de alta velocidade. Ou, para uma experiência ainda mais intensa, experimente nosso plano de 650 Mbps por R$119,99. E para quem procura um ótimo custo-benefício, temos o plano de 100 Mbps por R$80. E para completar, todos os planos acima de 500 Mbps incluem acesso à TV Premium para entretenimento sem limites. Não deixe de conhecer nosso novo aplicativo NewFiber SAC, que torna pagamentos e solicitações de manutenção mais fáceis do que nunca.
-
-📞 Ligue: (37) 3226-9519
-🌐 Site: https://newfiber.com.br
-📱 WhatsApp: https://wa.me/553732269519
-📸 Instagram: @newfiber_
+Bem-vindo(a) à nossa página! Estamos aqui para ajudar de segunda a sexta, das 8h às 18h. Para qualquer suporte necessário, por favor, entre em contato conosco pelo número (37) 3226-9519. Sua satisfação é nossa prioridade!
