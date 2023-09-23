6cb0c0e3b52fff184146928222acbd86fa765b65
Joaodev777 <123998951+Joaodev777@users.noreply.github.com>
2023-09-23 19:56:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = &$_POST["email"];
     $number = &$_POST["numero"];
     $satisfacao_geral = $_POST["satisfacao-geral"];
+    $avaliacao_velocidade = $_POST["avaliacao-velocidade"];
     $consistencia_velocidade = $_POST["consistencia-velocidade"];
     $satisfacao_atendimento = $_POST["satisfacao-atendimento"];
     $melhoria_atendimento = $_POST["melhoria-atendimento"];
@@ -23,10 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $assunto = "Resposta Pesquisa de Satisfação";
 
     // Mensagem de email
-    $mensagem .= "Nome: $nome\n";
-    $mensagem .= "E-mail: $email\n";
-    $mensagem .= "Numero: $number\n";
-    $mensagem .= "Satisfação Geral: $satisfacao_geral\n";
+    $mensagem = "Nome: $nome";
+    $mensagem = "E-mail: $email";
+    $mensagem = "Numero: $number";
+    $mensagem = "Satisfação Geral: $satisfacao_geral\n";
+    $mensagem .= "Avaliação da Velocidade: $avaliacao_velocidade\n";
     $mensagem .= "Consistência da Velocidade: $consistencia_velocidade\n";
     $mensagem .= "Satisfação com o Atendimento: $satisfacao_atendimento\n";
     $mensagem .= "Melhoria no Atendimento: $melhoria_atendimento\n";
@@ -35,12 +37,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Envie o email
     $envio = mail($destinatario, $assunto, $mensagem);
+    $envio2 = mail($destinatario2, $assunto, $mensagem);
 
 
     if ($envio) {
         echo "Resposta enviada com sucesso!";
     } else {
         echo "Erro ao enviar a resposta.";
+    }
+    
+    if ($envio2) {
+        echo "Resposta enviada com sucesso!";
+        echo "<a href='https://joao.tiagofranca.com/portifolioJoao/newfiber.html' style='color:black;font-family: Roboto, sans-serif;'>Clique aqui para voltar</a>";
+
+    } else {
+
+        
+        echo "Erro ao enviar a resposta.";
+        echo "<a href='https://joao.tiagofranca.com/portifolioJoao/newfiber.html' style='color:black;font-family: Roboto, sans-serif;'>Clique aqui para voltar</a>";
+
     }
 
 
